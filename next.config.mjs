5618a7a096cb24c7fe1125554f2721911a975e7d
Zenitramhgx <zenitramhgx15@hotmail.com>
2025-01-15 10:17:19 -0600

    Xura-12: fix: Configuraciones COMPLETE

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,4 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-//
-// export default nextConfig;
-// Quitar los comnentarios despues y borrar lo de abajo
-// Eliminar proxy del package.json
-
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: false,
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'http://192.168.8.206:3001/api/:path*', // Proxy del backend
-      },
-    ];
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
